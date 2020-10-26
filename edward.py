353c1885ea68a2546b3f35bbd020c79492fa8dfe
Anastasiya Taranava <taranova.nas@gmail.com>
2020-10-26 21:17:54 +0300

    new version

--- edward.py
+++ edward.py
@@ -1,23 +1,47 @@
-import re
 import sys
 
 
-def text_from_file():
-    with open(sys.argv[1], 'r') as f:
-        text = [line.replace(' ', '') for line in f]
-        return text
+def get_from_file():
+    file = sys.argv[1]
+    return file
 
 
-r = re.compile(r'([a-z])\1')
+def read_from_file(file):
+    text = open(file).read()
+    return text
 
 
-def remove_identical_letters(string):
-    text = r.sub(r'', string)
+def list_text(text):
+    list = [text[i] for i in range(len(text))]
+    return list
+
+
+def remove_duplicates(chars):
+    count = 0
+    while count < (len(chars) - 1):
+        if chars[count] == chars[count + 1]:
+            chars.pop(count)
+            chars.pop(count)
+            if count != 0:
+                count -= 1
+            continue
+        else:
+            count += 1
+    return chars
+
+
+def return_to_str(list):
+    str = ''.join(list)
+    return str
+
+
+def main():
+    infile = get_from_file()
+    text_from_file = read_from_file(infile)
+    chars = list_text(text_from_file)
+    no_duplicates = remove_duplicates(chars)
+    text = return_to_str(no_duplicates)
     print(text)
 
 
-if __name__ == '__main__':
-    content = text_from_file()
-
-    for string in content:
-        remove = remove_identical_letters(string)
+main()
